91c66f44c5854c64c80a4c8641fe9bf7385a57e0
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-11-02 20:21:18 +0700

    Book Management system complete api (Done)

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/NyanLinHtoo/Book-Management-System/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
